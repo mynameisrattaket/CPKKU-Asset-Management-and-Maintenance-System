e10063f128dfe492c6a281a702e7c23da1324f45
Kengelite <apipath90@gmail.com>
2024-05-03 14:43:31 +0700

    insert page index2

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 Route::get('/index', function () {
-    return view('index');
+    return view('welcome');
 });
