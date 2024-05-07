49ceb15184e17997162604b0edb1ba598ae1e309
RattaphoomT <rattaphoom.t@kkumail.com>
2024-05-07 16:23:10 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KarupanController;
 
-Route::POST('/',[KarupanController::class,'index']);
+Route::POST('/creat',[KarupanController::class,'index']);
 
 Route::get('/',[KarupanController::class,'index']);
 
