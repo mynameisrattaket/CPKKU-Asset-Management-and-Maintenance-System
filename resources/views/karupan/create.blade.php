002161f4400038c283184a47a6aaf5e12e0d0581
RattaphoomT <rattaphoom.t@kkumail.com>
2024-05-09 14:29:04 +0700

    deit_btn_close

--- resources/views/karupan/create.blade.php
+++ resources/views/karupan/create.blade.php
@@ -16,9 +16,7 @@
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h4 class="modal-title" id="exampleModalLabel" >Create New</h4>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
                                     <div class="col-md-2">
