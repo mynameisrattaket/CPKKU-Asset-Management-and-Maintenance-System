fc6f4d0534bde76f7a7ab99fa0b09edeed5499d4
RattaphoomT <rattaphoom.t@kkumail.com>
2024-05-10 11:05:48 +0700

    comit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -74,6 +74,7 @@
 
 @section('scripts')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <script>
     $(document).ready(function()
     {
@@ -91,6 +92,7 @@
         console.log(asset_id);
     });
 </script>
+
 @endsection
 
 
