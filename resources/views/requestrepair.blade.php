24ec4d7b9fa9fa96a0b1f151477f2ce4428f753a
mynameisrattaket <rattaket.w@kkumail.com>
2024-05-15 13:21:48 +0700

    -

--- resources/views/requestrepair.blade.php
+++ resources/views/requestrepair.blade.php
@@ -7,6 +7,23 @@
 @endsection
 
 @section('conten')
+
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+    @endif
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <!-- เพิ่มฟอร์มตรงนี้ -->
     <form action="{{ route('addrequestrepair') }}" method="POST">
         @csrf
