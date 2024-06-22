dc192cf5e0ee7e89845b952768b54b2ca34b6c92
mynameisrattaket <rattaket.w@kkumail.com>
2024-06-22 14:32:09 +0700

    -

--- resources/views/layoutmenu.blade.php
+++ resources/views/layoutmenu.blade.php
@@ -185,7 +185,7 @@
                     <li class="side-nav-item">
                         <a href="{{ route('repaircancel') }}" class="side-nav-link">
                             <i class="uil-multiply"></i>
-                            <span> ถูกยกเลิก </span>
+                            <span> ซ่อมไม่ได้ </span>
                         </a>
                     </li>
 
@@ -609,7 +609,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    
+
 
 
 
