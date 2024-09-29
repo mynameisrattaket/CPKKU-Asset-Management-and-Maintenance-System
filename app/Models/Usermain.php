7f95ee9b4c56b9fef3d0b547cf30f7d0773a495a
mynameisrattaket <rattaket.w@kkumail.com>
2024-09-30 01:15:44 +0700

    Update Usermain.php

--- app/Models/Usermain.php
+++ app/Models/Usermain.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class Usermain extends Model
 {
@@ -11,19 +12,24 @@ class Usermain extends Model
 
     protected $table = 'user';
     protected $primaryKey = 'id';
+
+    // หากไม่ใช้ timestamps อัตโนมัติให้ตั้งค่าเป็น false
+    public $timestamps = false;
+
     protected $fillable = [
         'name',
         'email',
         'password',
         'user_status',
-        'user_created_at',
-        'user_update_at',
-        'user_Last_login_at',
-        'user_delete_at',
         'user_type',
         'user_type_id',
         'email_verified_at',
         'user_major'
-
     ];
+
+    // แทนที่การบันทึกรหัสผ่านเพื่อทำการ Hash
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
 }
