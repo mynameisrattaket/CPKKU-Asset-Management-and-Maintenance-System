9a8336e7e76ac09344ca59734e24d12209a31af0
mynameisrattaket <rattaket.w@kkumail.com>
2024-12-25 04:24:24 +0700

    ค้นหาข้อมูลคุรุภัณ

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,35 +11,55 @@
     <!-- ฟอร์มสำหรับค้นหา -->
     <form id="searchForm" action="{{ route('searchasset') }}" method="GET" class="mb-3">
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="ค้นหาข้อมูลครุภัณฑ์" aria-label="ค้นหาข้อมูลครุภัณฑ์" aria-describedby="button-addon2" name="searchasset">
-            <button class="btn btn-primary" type="submit" id="button-addon2">ค้นหา</button>
+            <input type="text" class="form-control" placeholder="ค้นหาข้อมูลครุภัณฑ์" name="searchasset">
+            <button class="btn btn-primary" type="submit">ค้นหา</button>
         </div>
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="หมายเลขครุภัณฑ์" aria-label="หมายเลขครุภัณฑ์" aria-describedby="button-addon2" name="asset_number">
-            <input type="text" class="form-control" placeholder="ราคาต่อหน่วย" aria-label="ราคาต่อหน่วย" aria-describedby="button-addon2" name="asset_price">
-            <input type="text" class="form-control" placeholder="สถานะ" aria-label="สถานะ" aria-describedby="button-addon2" name="asset_asset_status_id">
-            <input type="text" class="form-control" placeholder="คอมเมนต์" aria-label="คอมเมนต์" aria-describedby="button-addon2" name="asset_comment">
+        <div class="input-group mt-2">
+            <input type="text" class="form-control" placeholder="ไอดี" name="asset_id">
+            <input type="text" class="form-control" placeholder="หมายเลขครุภัณฑ์" name="asset_number">
+            <input type="text" class="form-control" placeholder="ราคาต่อหน่วย" name="asset_price">
+            <input type="text" class="form-control" placeholder="สถานะ" name="asset_asset_status_id">
+        </div>
+        <div class="input-group mt-2">
+            <input type="text" class="form-control" placeholder="ยี่ห้อ" name="asset_brand">
+            <input type="text" class="form-control" placeholder="ปีงบประมาณ" name="asset_budget">
+        </div>
+        <div class="input-group mt-2">
+            <input type="text" class="form-control" placeholder="แหล่งเงิน" name="asset_fund">
+            <input type="text" class="form-control" placeholder="สถานที่" name="asset_location">
+            <input type="text" class="form-control" placeholder="วิธีการได้มา" name="asset_reception_type">
         </div>
     </form>
+
     <!-- ตารางแสดงผลลัพธ์การค้นหา -->
-    <table class="table">
+    <table id="assetTable" class="table table-bordered table-sm mt-4">
         <thead>
             <tr>
-                <th>ชื่อครุภัณฑ์</th>
+                <th>ไอดี</th>
                 <th>หมายเลขครุภัณฑ์</th>
+                <th>ชื่อครุภัณฑ์</th>
                 <th>ราคาต่อหน่วย</th>
+                <th>ยี่ห้อ</th>
                 <th>สถานะ</th>
-                <th>คอมเมนต์</th>
+                <th>ปีงบประมาณ</th>
+                <th>แหล่งเงิน</th>
+                <th>สถานที่</th>
+                <th>วิธีการได้มา</th>
             </tr>
         </thead>
         <tbody>
             @foreach($asset_main as $karupan)
             <tr>
-                <td>{{ $karupan->asset_name }}</td>
+                <td>{{ $karupan->asset_id }}</td>
                 <td>{{ $karupan->asset_number }}</td>
+                <td>{{ $karupan->asset_name }}</td>
                 <td>{{ $karupan->asset_price }}</td>
+                <td>{{ $karupan->asset_brand }}</td>
                 <td>{{ $karupan->asset_asset_status_id }}</td>
-                <td>{{ $karupan->asset_comment }}</td>
+                <td>{{ $karupan->asset_budget }}</td>
+                <td>{{ $karupan->asset_fund }}</td>
+                <td>{{ $karupan->asset_location }}</td>
+                <td>{{ $karupan->asset_reception_type }}</td>
             </tr>
             @endforeach
         </tbody>
