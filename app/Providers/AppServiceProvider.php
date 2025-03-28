b4b32d7885bd498208c564e9841817c7659836a1
mynameisrattaket <rattaket.w@kkumail.com>
2025-03-28 23:07:05 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
         // ผูก middleware
         $router = $this->app['router'];
         $router->aliasMiddleware('check_user_type', \App\Http\Middleware\CheckUserType::class);
