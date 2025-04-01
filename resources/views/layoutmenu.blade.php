491d59c87ed46982083d8bd6333e4871b775322d
mynameisrattaket <rattaket.w@kkumail.com>
2025-04-01 17:46:50 +0700

    เพิิ่ม preview ภาพเเละป้องกัน user ซ้ำ

--- resources/views/layoutmenu.blade.php
+++ resources/views/layoutmenu.blade.php
@@ -29,7 +29,6 @@
     <!-- App css -->
     <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="light-style">
-    <link href="{{ asset('assets/css/app-dark.min.css') }}" rel="stylesheet" type="text/css" id="dark-style">
     {{-- Font --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
