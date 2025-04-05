32d43df583a95210c0370397d42c05a6ddf8dd6b
mynameisrattaket <rattaket.w@kkumail.com>
2025-04-05 16:12:29 +0700

    เพิ่ม vite ให้เว็บเร็วขึ้น

--- resources/views/layoutmenu.blade.php
+++ resources/views/layoutmenu.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
